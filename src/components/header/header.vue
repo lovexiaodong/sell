5d95c6958c6d173c084f6962ac85fcfb4948749c
william.zhang <zydqlg@sina.com>
2017-08-28 19:25:40 +0800

    finished the header.vue

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,14 +29,35 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%"/>
     </div>
-    <div v-show="detailShow" class="detail">
+    <div v-show="detailShow" class="detail" transition name="fade">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
-          <star :size='48' :score="seller.score" class="star"></star>
+          <div class="star">
+            <star :size='48' :score="seller.score" class="star"></star>
+          </div>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text"> 优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul v-if="seller.supports" class="supports">
+            <li  v-for="item in seller.supports"  v-bind:key="item.type" class="supports-item">
+              <span class="icon" :class="classMap[item.type]"></span>
+              <span class="text">{{item.description}}</span>
+            </li>
+          </ul>
+           <div class="title">
+            <div class="line"></div>
+            <div class="text"> 商家公告</div>
+            <div class="line"></div>
+          </div>
+          <div class="bulletin">
+            <p class="content">{{seller.bulletin}}</p>
+          </div>
         </div>
       </div>
-      <div class="detail-closed">
+      <div class="detail-closed" @click="hidenDetail">
         <i class="icon-close"></i>
       </div>
     </div>
@@ -45,6 +66,7 @@
 
 <script >
   import star from '../star/star.vue';
+
   export default {
     props: {
       seller: {
@@ -62,6 +84,9 @@
     methods: {
       showDetail: function () {
         this.detailShow = true;
+      },
+      hidenDetail: function () {
+        this.detailShow = false;
       }
     },
     created() {
@@ -192,7 +217,13 @@
       height: 100%
       z-index: 100
       overflow: auto
+      transition: all 0.5s
       background: rgba(7, 17, 27, 0.8)
+      &.fade-transition
+        opacity: 1
+      &.fade-enter &.fade-leave
+        opacity: 0
+        background: rgba(7, 17, 27, 0)
       .detail-wrapper
         min-height: 100%
         width: 100%
@@ -207,7 +238,60 @@
             text-align: center
           .star
             margin-top: 16px
+            font-size: 24px
             text-align: center
+          .title
+            display: flex
+            margin: 30px auto 24px auto
+            width: 80%
+            .line
+              flex: 1
+              top: -6px
+              position: relative
+              border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+            .text
+              flex: 1
+              text-align: center
+              padding: 0 12px
+              font-size: 14px   
+          .supports
+            width: 80%
+            margin: 0 auto
+            .supports-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height: 16px
+                font-size: 12px    
+          .bulletin
+            width: 80% 
+            margin: 0 auto
+            .content
+              line-height: 24px
+              font-size: 12px
+              font-weight: 200
+                    
       .detail-closed
          position: relative
          margin: -64px auto 0 auto
